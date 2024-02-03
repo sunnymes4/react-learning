9209e88c4ca1b1bf125eb14d0e7276124436f6d1
sunnymes4 <sunnymes4@gmail.com>
2024-02-04 01:19:41 +0530

    created post card view and Auth layout to render children view conditionaly for pre and post login pages

---
+++ blog-appwrite/src/components/AuthLayout.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
+export default function AuthLayout({children, authentication = true}) {
+    const authStatus = useSelector(state => state.auth.status)
+    const [loading, setLoading] = useState(true)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(authentication && authStatus !== authentication) {
+            navigate('/login')
+        } else if(!authentication && authStatus !== authentication) {
+            navigate('/')
+        }
+
+        setLoading(false)
+    }, [authStatus, navigate, authentication])
+    
+    return loading ?  <div>Loading...</div> : <>{children}</>
+}
--- blog-appwrite/src/components/PostCard.jsx
+++ blog-appwrite/src/components/PostCard.jsx
@@ -11,12 +11,12 @@ function PostCard({
     return (
         
         <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-200 rounded-lg p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={imgPreview} alt={title} className='rounded-xl'/>
+            <div className='w-full bg-gray-200 rounded-lg p-4'>
+                <div className='w-full justify-center mb-4'>
+                    <img src={imgPreview} alt={title} className='rounded-xl'/>
+                </div>
+                <h2 className='text-xl font-bold'>{title}</h2>
             </div>
-            <h2 className='text-xl font-bold'>{title}</h2>
-        </div>
         </Link>
     )
 }
